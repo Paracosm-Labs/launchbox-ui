31459a46c93c663bcc3b0be680af91f765487ef3
CosmicAnon <wescosmic@gmail.com>
2023-07-11 17:49:17 +0545

    card and nav updates

--- src/components/course.js
+++ src/components/course.js
@@ -1,51 +1,58 @@
 import React from 'react';
 
-function Course({}) {
+function Course() {
+
+// Dummy data
+const courses = [
+  {
+    id:1,
+    title: '360 Photography',
+    img: 'https://media.discordapp.net/attachments/1061348902252597368/1127958996192600116/wescosmic_ui_card_content_display_for_game_choice_of_a_caribbea_f413581f-ceed-4983-affb-03e49631cefb.png?width=662&height=662',
+    description: 'This is course 1',
+    link: '#Start1'
+  },
+  {
+    id:2,
+    title: 'Drone Photography',
+    img: 'https://media.discordapp.net/attachments/1061348902252597368/1127958995131437146/wescosmic_ui_card_content_display_for_game_choice_of_a_caribbea_f413581f-ceed-4983-affb-03e49631cefb.png?width=662&height=662',
+    description: 'This is course 2',
+    link: '#Start2'
+  },
+  {
+    id:3,
+    title: '3D Printing',
+    img: 'https://media.discordapp.net/attachments/1061348902252597368/1127958995131437146/wescosmic_ui_card_content_display_for_game_choice_of_a_caribbea_f413581f-ceed-4983-affb-03e49631cefb.png?width=662&height=662',
+    description: 'This is course 3',
+    link: '#Start3'
+  },
+
+  // Add more courses as needed
+];
+
+
   return (<>
       
     <div className="row mt-5">
 
-
+    {courses.map((course, index) => (
       <div className="col-3 mb-4">
-        <div className="card course-card">
-          <img src="https://media.discordapp.net/attachments/1061348902252597368/1127958996192600116/wescosmic_ui_card_content_display_for_game_choice_of_a_caribbea_f413581f-ceed-4983-affb-03e49631cefb.png?width=662&height=662" className="img-fluid card-img-top" alt="..." />
-          <div className="card-body text-dark">
-            <h5 className="card-title">360 Photography</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Start Now</a>
+        <div key={index} className="card course-card">
+          <img src={course.img} className="img-fluid card-img-top" alt="..." />
+          <div className="card-body text-dar">
+            <h5 className="card-title">{course.title}</h5>
+            <p className="card-text">{course.description}</p>
+            <a href={course.link} className="btn btn-primary">Start Now</a>
           </div>
         </div>
       </div>
+    ))}
+
 
       <div className="col-3 mb-4">
         <div className="card course-card">
-        <img src="https://media.discordapp.net/attachments/1061348902252597368/1127958995131437146/wescosmic_ui_card_content_display_for_game_choice_of_a_caribbea_f413581f-ceed-4983-affb-03e49631cefb.png?width=662&height=662" className="img-fluid card-img-top" alt="..." />
-          <div className="card-body text-dark">
-            <h5 className="card-title">3D Printing</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Start Now</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-3 mb-4">
-        <div className="card course-card">
-        <img src="https://media.discordapp.net/attachments/1061348902252597368/1127958995131437146/wescosmic_ui_card_content_display_for_game_choice_of_a_caribbea_f413581f-ceed-4983-affb-03e49631cefb.png?width=662&height=662" className="img-fluid card-img-top" alt="..." />
-          <div className="card-body text-dark">
-            <h5 className="card-title">Drone Photography</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Start Now</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-3 mb-4">
-        <div className="card course-card">
-
-          <div className="card-body text-dark mt-5">
-            <h5 className="card-title">Custom</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-outline-primary">Create Your Own</a>
+          <div className="card-body text-dar mt-5">
+            <h5 className="card-title">Launch Your Own</h5>
+            <a href="#" className="btn btn-outline-success mt-5">Start Now</a>
           </div>
         </div>
       </div>
--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -7,9 +7,9 @@ function Sidenav() {
     <div className="mb-5">
       <h4>LaunchBox</h4><hr/>
     </div>
-      <Link to="/" className="nav-link"><i className="fa-solid fa-play mx-2"></i>Play</Link><br/>
+      <Link to="/" className="nav-link"><i className="fa-solid fa-play mx-2"></i>Learn</Link><br/>
       <Link to="/workspace" className="nav-link"><i className="fa-solid fa-message mx-2"></i>Workspace</Link><br/>
-      <Link to="/staking" className="nav-link"><i className="fa-solid fa-charging-station mx-2"></i>Staking</Link>
+      <Link to="/staking" className="nav-link"><i className="fa-solid fa-atom mx-2"></i>Upgrades</Link>
     </nav>
   );
 }
